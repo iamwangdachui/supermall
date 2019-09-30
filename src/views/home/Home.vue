4f5e76f7fcf1561b6b4b7a8a320619f8a5ed0d27
zhuchunbo <1784551003@qq.com>
2019-09-30 15:43:47 +0800

    本周流行

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,6 +8,20 @@
     <home-swiper :banners="banners"></home-swiper>
     <!--推荐-->
     <recommend-view :recommends="recommends"></recommend-view>
+    <!--本周推荐-->
+    <feature-view></feature-view>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
@@ -15,6 +29,7 @@
 import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 import { getHomeMultidata } from "../../network/home";
 
 export default {
@@ -22,7 +37,8 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
@@ -42,8 +58,16 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 40px;
+}
 .home-nav {
   background-color: #f5617b;
   color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 </style>
